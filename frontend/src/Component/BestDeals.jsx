f96b4cb12daf0dae460610cd1ce1f7dc4ebae238
Nahid <syed.ah.nahid@gmail.com>
2024-10-20 23:56:56 +0600

    best deals stracture

--- frontend/src/Component/BestDeals.jsx
+++ frontend/src/Component/BestDeals.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 function BestDeals() {
   return (
-    <div>BestDeals</div>
+    <div className='w-screen h-screen px-20 flex flex-row'>
+        <div className='w-1/3'>
+            sale
+        </div>
+        <div className='w-1/3 flex flex-col'>
+            <div className='h-2/3'>
+                best category
+            </div>
+            <div className='h-1/3'>
+                2nd best category
+            </div>
+        </div>
+        <div className='w-1/3 flex flex-col'>
+            <div className='h-1/3'>
+                3rd best category
+            </div>
+            <div className='h-1/3'>
+                4th best category
+            </div>
+            <div className='h-1/3'>
+                new collections
+            </div>
+        </div>
+
+    </div>
   )
 }
 
