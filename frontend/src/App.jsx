c91ac23adbbde27ae34e392256197e3e540bf07c
Nahid <syed.ah.nahid@gmail.com>
2024-10-20 12:57:00 +0600

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+        <main className='py-3'>
+            <Routes>
+                
+            </Routes>
+        </main>
+        </Router>
+    );
+}
+
+export default App;
